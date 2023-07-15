d8a2d904d11a6cb7d05e585fbe447e8945fe7550
Ghu1iyev <tu12n4c1r@code.edu.az>
2023-07-15 21:27:48 +0400

    upload image

--- app.js
+++ app.js
@@ -1,46 +1,78 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
-const mongoose = require('mongoose')
-const Photo = require('./models/Photo')
+const fs = require('fs')
+const Photo = require('./models/Photo');
+
 const app = express();
 
 //Connect DB
-mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db',{
+mongoose
+  .connect('mongodb://127.0.0.1:27017/pcat-test-db', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log('DB Connected Successfuly');
-}).catch(err => console.log(err))
+  })
+  .catch((err) => console.log(err));
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(fileUpload());
 
 //Routes
-app.get('/', async(req, res) => {
-    const photos = await Photo.find({})
-  res.render('index',{
-    photos
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
   });
 });
 
-app.get('/about', (req,res) => {
-    res.render('about')
-})
+app.post('/photos', async (req, res) => {
+    const uploadDir = 'public/uploads';
+    if(!fs.existsSync(uploadDir)){
+        fs.mkdirSync(uploadDir)
+    }
 
-app.get('/add', (req,res) => {
-    res.render('add')
-})
+    let uploadImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadImage.name
 
-app.post('/photos',async (req,res) => {
-   await Photo.create(req.body)
-    res.redirect('/')
-})
+    uploadImage.mv(uploadPath, 
+        async () => {
+            await Photo.create({
+                ...req.body,
+                image: '/uploads/' + uploadImage.name
+            })
+            res.redirect('/')
+        }
+    )
+    
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+
 
 app.listen(3000, () => {
   console.log('Server is started');
